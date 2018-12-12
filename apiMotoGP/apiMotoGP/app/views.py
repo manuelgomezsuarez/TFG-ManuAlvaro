9eabc80552a627658e0d6558a3c65f697a1c393e
Garbancito <Garbancito@GARBANCITO-PC>
2018-12-12 18:06:52 +0100

    Se ponen los nuevos filtrosen las querys

--- apiMotoGP/apiMotoGP/app/views.py
+++ apiMotoGP/apiMotoGP/app/views.py
@@ -1,75 +1,97 @@
 from django.shortcuts import render
- 
 from rest_framework_mongoengine import viewsets as meviewsets
 from apiMotoGP.serializers import PosicionCarreraSerializer, PosicionCampeonatoSerializer, PosicionDocumentacionSerializer
 from app.models import  Carreras, Campeonatos, Documentacion
+import django_filters.rest_framework
+
 
 class PosicionCarreraViewSet(meviewsets.ModelViewSet):
     serializer_class = PosicionCarreraSerializer
+    my_filter_fields = ('piloto', 'num','temporada','categoria','abreviatura','titulo','lugar','fecha','pos','puntos','pais','equipo','moto') # specify the fields on which you want to filter
+    #filter_backends = (filters.OrderingFilter,)
+    #ordering_fields = ('username', 'email')
+    def get_kwargs_for_filtering(self):
+        filtering_kwargs = {} 
+        for field in  self.my_filter_fields: # iterate over the filter fields
+            field_value = self.request.query_params.get(field) # get the value of a field from request query parameter
+            if field_value: 
+                filtering_kwargs[field] = field_value
+        return filtering_kwargs 
 
     def get_queryset(self):
-        queryset=Carreras.objects.all()
-        temporadaUrl= self.request.query_params.get('temporada', None)
-        if temporadaUrl is not None:
-            queryset = queryset.filter(temporada=temporadaUrl)
-
-        categoriaUrl = self.request.query_params.get('categoria', None)
-        if categoriaUrl is not None:
-            queryset = queryset.filter(categoria=categoriaUrl)
-
-        abreviaturaUrl = self.request.query_params.get('abreviatura', None)
-        if abreviaturaUrl is not None:
-            queryset = queryset.filter(abreviatura=abreviaturaUrl)
-
-        tituloUrl = self.request.query_params.get('titulo', None)
-        if tituloUrl is not None:
-            queryset = queryset.filter(titulo=tituloUrl)
-
-        lugarUrl = self.request.query_params.get('lugar', None)
-        if lugarUrl is not None:
-            queryset = queryset.filter(lugar=lugarUrl)
-
-        fechaUrl = self.request.query_params.get('fecha', None)
-        if lugarUrl is not None:
-            queryset = queryset.filter(fecha=fechaUrl)
-
-        posUrl = self.request.query_params.get('pos', None)
-        if posUrl is not None:
-            queryset = queryset.filter(pos=posUrl)
-
-        puntosUrl = self.request.query_params.get('puntos', None)
-        if puntosUrl is not None:
-            queryset = queryset.filter(puntos=puntosUrl)
-
-        numUrl = self.request.query_params.get('num', None)
-        if numUrl is not None:
-            queryset = queryset.filter(num=numUrl)
-
-        pilotoUrl = self.request.query_params.get('piloto', None)
-        if pilotoUrl is not None:
-            queryset = queryset.filter(piloto=pilotoUrl)
-
-        paisUrl = self.request.query_params.get('pais', None)
-        if paisUrl is not None:
-            queryset = queryset.filter(pais=paisUrl)
-
-        equipoUrl = self.request.query_params.get('equipo', None)
-        if equipoUrl is not None:
-            queryset = queryset.filter(equipo=equipoUrl)
-
-        motoUrl = self.request.query_params.get('moto', None)
-        if motoUrl is not None:
-            queryset = queryset.filter(moto=motoUrl)
-
-        kmhUrl = self.request.query_params.get('kmh', None)
-        if kmhUrl is not None:
-            queryset = queryset.filter(kmh=kmhUrl)
-
-        diferenciaUrl = self.request.query_params.get('diferencia', None)
-        if diferenciaUrl is not None:
-            queryset = queryset.filter(diferencia=diferenciaUrl)
-
+        queryset = Carreras.objects.all() 
+        filtering_kwargs = self.get_kwargs_for_filtering() # get the fields with values for filtering 
+        if filtering_kwargs:
+            queryset = Carreras.objects.filter(**filtering_kwargs) # filter the queryset based on 'filtering_kwargs'
         return queryset
+
+
+#class PosicionCarreraViewSet(meviewsets.ModelViewSet):
+#    serializer_class = PosicionCarreraSerializer
+
+#    def get_queryset(self):
+#        queryset=Carreras.objects.all()
+#        temporadaUrl= self.request.query_params.get('temporada', None)
+#        if temporadaUrl is not None:
+#            queryset = queryset.filter(temporada=temporadaUrl)
+
+#        categoriaUrl = self.request.query_params.get('categoria', None)
+#        if categoriaUrl is not None:
+#            queryset = queryset.filter(categoria=categoriaUrl)
+
+#        abreviaturaUrl = self.request.query_params.get('abreviatura', None)
+#        if abreviaturaUrl is not None:
+#            queryset = queryset.filter(abreviatura=abreviaturaUrl)
+
+#        tituloUrl = self.request.query_params.get('titulo', None)
+#        if tituloUrl is not None:
+#            queryset = queryset.filter(titulo=tituloUrl)
+
+#        lugarUrl = self.request.query_params.get('lugar', None)
+#        if lugarUrl is not None:
+#            queryset = queryset.filter(lugar=lugarUrl)
+
+#        fechaUrl = self.request.query_params.get('fecha', None)
+#        if lugarUrl is not None:
+#            queryset = queryset.filter(fecha=fechaUrl)
+
+#        posUrl = self.request.query_params.get('pos', None)
+#        if posUrl is not None:
+#            queryset = queryset.filter(pos=posUrl)
+
+#        puntosUrl = self.request.query_params.get('puntos', None)
+#        if puntosUrl is not None:
+#            queryset = queryset.filter(puntos=puntosUrl)
+
+#        numUrl = self.request.query_params.get('num', None)
+#        if numUrl is not None:
+#            queryset = queryset.filter(num=numUrl)
+
+#        pilotoUrl = self.request.query_params.get('piloto', None)
+#        if pilotoUrl is not None:
+#            queryset = queryset.filter(piloto=pilotoUrl)
+
+#        paisUrl = self.request.query_params.get('pais', None)
+#        if paisUrl is not None:
+#            queryset = queryset.filter(pais=paisUrl)
+
+#        equipoUrl = self.request.query_params.get('equipo', None)
+#        if equipoUrl is not None:
+#            queryset = queryset.filter(equipo=equipoUrl)
+
+#        motoUrl = self.request.query_params.get('moto', None)
+#        if motoUrl is not None:
+#            queryset = queryset.filter(moto=motoUrl)
+
+#        kmhUrl = self.request.query_params.get('kmh', None)
+#        if kmhUrl is not None:
+#            queryset = queryset.filter(kmh=kmhUrl)
+
+#        diferenciaUrl = self.request.query_params.get('diferencia', None)
+#        if diferenciaUrl is not None:
+#            queryset = queryset.filter(diferencia=diferenciaUrl)
+
+#        return queryset
     
 class PosicionCampeonatoViewSet(meviewsets.ModelViewSet):
     serializer_class = PosicionCampeonatoSerializer
