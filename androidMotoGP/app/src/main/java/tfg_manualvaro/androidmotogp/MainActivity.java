f1bbe6a3f42f0f90662d8f27cfe21bee9ccb2830
manuelgomezsuarez@gmail.com <manuelgomezsuarez@gmail.com>
2019-02-16 13:42:29 +0100

    Primera Request Con Volley json to String

--- androidMotoGP/app/src/main/java/tfg_manualvaro/androidmotogp/MainActivity.java
+++ androidMotoGP/app/src/main/java/tfg_manualvaro/androidmotogp/MainActivity.java
@@ -2,25 +2,73 @@ package tfg_manualvaro.androidmotogp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
     }
 
-    public void Buscar(View view) {
-        String busqueda;
-        busqueda = ((EditText)findViewById(R.id.textInput1)).getText().toString();
+    private void jsonParse() {
 
-        TextView texto1= (TextView)findViewById(R.id.texto1);
-        texto1.setText(busqueda);
-        texto1.setVisibility(View.VISIBLE);
+        String url = "http://hr8jeljvudseiccl8kzsu4.webrelay.io/distintos/?format=json";
 
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("results");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject temporada = jsonArray.getJSONObject(i);
+
+                                String anosParaFiltro = temporada.getString("temporada");
+                                Log.i("Temporada", anosParaFiltro);
+                                mTextViewResult.append(anosParaFiltro+"\n\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
     }
 }
