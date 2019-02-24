3009b65a6a9f69f837adae4377755ae83507acb3
manuelgomezsuarez@gmail.com <manuelgomezsuarez@gmail.com>
2019-02-24 20:18:11 +0100

    Primer Listado de Años

--- androidMotoGP/app/src/main/java/tfg_manualvaro/androidmotogp/MainActivity.java
+++ androidMotoGP/app/src/main/java/tfg_manualvaro/androidmotogp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -18,17 +19,25 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String KEY_SUCCESS = "count";
     private static final String KEY_DATA = "results";
     private static final String KEY_TEMPORADA = "temporada";
+    private static final String KEY_NEXT = "next";
 
-    private String url = "http://hr8jeljvudseiccl8kzsu4.webrelay.io/campeonato/?distinct=temporada&format=json";
+    private String url = "http://hr8jeljvudseiccl8kzsu4.webrelay.io/campeonato/";
+    private Map<String,String> urlParams= new HashMap<>();
+
     private ProgressDialog pDialog;
     private int success;
+    private String next;
+    private Integer pagination=1;
     private TemporadaAdapter adapter;
+    private JSONArray temporadasJSONArray=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +64,32 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
             HttpJsonParser jsonParser = new HttpJsonParser();
-            response = jsonParser.makeHttpRequest(url,"GET",null);
+            urlParams.put("format","json");
+            urlParams.put("distinct","temporada");
+            urlParams.put("page",pagination.toString());
+
+            response = jsonParser.makeHttpRequest(url,"GET",urlParams);
             try {
                 success = response.getInt(KEY_SUCCESS);
+                next=response.getString(KEY_NEXT);
+                temporadasJSONArray =  response.getJSONArray(KEY_DATA);
+                while(next!="null"){
+                    pagination=pagination+1;
+                    urlParams.put("page",pagination.toString());
+                    Log.i("next",pagination.toString());
+                    response = jsonParser.makeHttpRequest(url,"GET",urlParams);
+                    next=response.getString(KEY_NEXT);
+                    Log.i("next",next);
+                    JSONArray temporadasJSONArrayNext =  response.getJSONArray(KEY_DATA);
+
+                    for (int i = 0; i < temporadasJSONArrayNext.length(); i++) {
+
+                        JSONObject jsonObject = temporadasJSONArrayNext.getJSONObject(i);
+                        temporadasJSONArray.put(jsonObject);
+                    }
+                }
+                Log.i("next", temporadasJSONArray.toString());
+                pagination=1;
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -70,10 +102,9 @@ public class MainActivity extends AppCompatActivity {
                 public void run() {
 
                     ListView listView =(ListView)findViewById(R.id.temporadaList);
-                    if (success == 1) {
+                    if (success >0) {
                         try {
                             System.out.println(KEY_DATA);
-                            JSONArray temporadasJSONArray =  response.getJSONArray(KEY_DATA);
                             List<Temporada> temporadasList = new ArrayList<>();
                             //Populate the EmployeeDetails list from response
                             for (int i = 0; i<temporadasJSONArray.length();i++){
